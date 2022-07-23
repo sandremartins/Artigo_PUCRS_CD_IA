5375c5d96543d5b200672a8d44ae0cc57fc7c620
Alessandre Martins <sandremartins@gmail.com>
2022-07-23 14:16:53 -0400

    Analise exploratória de dados

--- airflow/dags/etlSatisfactionEvaluation.py
+++ airflow/dags/etlSatisfactionEvaluation.py
@@ -58,8 +58,11 @@ def load():
     #ler os dados a partir da área de Staging.
     df_ = pd.read_csv("/tmp/employeePerformanceEvaluation.csv")
 
+    #cria o PerfScoreFullyMeets para classificação dos que atendem o requisito
+    df_['perfScoreSatisfaction'] = df_[['PerformanceScore', 'EmpSatisfaction', 'Termd']].apply(lambda x: perfScoreSatisfaction(x), axis=1)
+
     #converte os dados para o formato parquet e pesiste na área de staging.
-    df_[['EmpID', 'EmpSatisfaction', 'PerfScoreID', 'PerformanceScore', 'EngagementSurvey']].to_parquet(
+    df_[['EmpID', 'EmpSatisfaction', 'PerfScoreID', 'PerformanceScore', 'EngagementSurvey', 'perfScoreSatisfaction']].to_parquet(
             "/tmp/employeePerformanceEvaluation.parquet"
             ,index=False
     )
@@ -71,7 +74,11 @@ def load():
         "/tmp/employeePerformanceEvaluation.parquet"
     )
 
-
+def perfScoreSatisfaction(x):
+    if (x['PerformanceScore'] == 'Fully Meets' or x['PerformanceScore'] == 'Needs Improvement') and (x['EmpSatisfaction'] == 3 or x['EmpSatisfaction'] == 4 or x['EmpSatisfaction'] == 5) and x['Termd'] == 0:
+        return True
+    else:
+        return False
 
 extract_task = PythonOperator(
     task_id='extractFileFromDataLake',
